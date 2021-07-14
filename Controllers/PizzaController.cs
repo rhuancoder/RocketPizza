4c7773acc2063d27f90e27481f769f8bab1d251b
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-07-14 19:54:34 -0300

    Adding Pizza Get endpoint

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -3,19 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 using RocketPizza.Models;
 using RocketPizza.Services;
 
-namespace RocketPizza.Controllers {
+namespace RocketPizza.Controllers
+{
     [ApiController]
     [Route("[controller]")]
-    public class PizzaController : ControllerBase {
+    public class PizzaController : ControllerBase
+    {
         public PizzaController()
         {
-            
+
         }
 
         [HttpGet]
         public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
 
-        // GET by Id
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
 
         // POST
 
