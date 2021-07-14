56e77a8cfa7c913438da804598c980f180083567
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2021-07-14 19:48:06 -0300

    Adding new Controller

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RocketPizza.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase {
+        public PizzaController()
+        {
+            
+        }
+
+        // GET all
+
+        // GET by Id
+
+        // POST
+
+        // PUT
+
+        // DELETE
+    }
+}
